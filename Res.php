6665253439a417c82d7ce89b781e86fc147df354
Raed <raed.riseup.net>
2015-05-02 20:18:39 +0100

    Revert to f0a01a9f6361c677914dd5fc823763ddffdd36c6

---
+++ Res.php
@@ -0,0 +1,13 @@
+<?php
+
+class Res {
+
+    public $pic;
+    public $ratio;
+
+    public function __construct($pic, $ratio) {
+        $this->pic = $pic;
+        $this->ratio = $ratio;
+    }
+
+}
