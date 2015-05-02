35ae9c355972190365c5be78df71be45af6105e1
Raed <raed.riseup.net>
2015-05-02 14:46:09 +0100

    added front cmp

---
+++ public_html/js/main.js
@@ -0,0 +1,108 @@
+function showValue(newValue)
+{
+    document.getElementById("range").innerHTML = newValue;
+    $('.img-result').each(function (i, obj) {
+        var val = parseFloat($(this).attr("alt"));
+        val *= 100;
+        if (newValue <= val) {
+            $(this).show();
+        } else {
+            $(this).hide();
+        }
+    });
+}
+
+function check() {
+    var x = document.getElementById("myCheck1").checked;
+    var y = document.getElementById("myCheck2").checked;
+    var z = document.getElementById("myCheck3").checked;
+    var range = document.getElementById("slide").value;
+    alert("Checkbox1 : " + x + "\n" + "Checkbox2 : " + y + "\n" + "Checkbox3 : " + z + "\n" + "Range :" + range);
+    $('.img-results').html('');
+    /// HISTOGRAMME
+    if (x)
+    {
+        alert($('img[id="img-chosen"]').attr('alt'));
+        $.post("http://localhost/cmp/worker.php",
+                {
+                    id: "" + $('img[id="img-chosen"]').attr('alt')
+                },
+        function (data, status) {
+
+            var obj = jQuery.parseJSON(data);
+            for (var i = 0, max = obj.length; i < max; i++) {
+
+                var disp = "display: inline;";
+                var rat = parseFloat(obj[i].ratio) * 100;
+                if (range > rat) {
+                    disp = "display: none;";
+                }
+
+                $('#Hist').append($('<img>', {style: disp, alt: obj[i].ratio, id: "found_" + obj[i].pic, class: "img-result", src: 'http://localhost/cmp/dataset/' + obj[i].pic}));
+            }
+
+        });
+    }
+
+/// FORMES
+
+    if (y)
+    {
+        var val = range / 100;
+        alert($('img[id="img-chosen"]').attr('alt'));
+        $.post("http://localhost/cmp/worker2.php",
+                {
+                    id: "" + $('img[id="img-chosen"]').attr('alt'),
+                    ratio: val
+                },
+        function (data, status) {
+
+            var obj = jQuery.parseJSON(data);
+            for (var i = 0, max = obj.length; i < max; i++) {
+
+                var disp = "display: inline;";
+                var rat = parseFloat(obj[i].ratio) * 100;
+                if (range > rat) {
+                    disp = "display: none;";
+                }
+
+                $('#Form').append($('<img>', {style: disp, alt: obj[i].ratio, id: "found_" + obj[i].pic, class: "img-result", src: 'http://localhost/cmp/dataset/' + obj[i].pic}));
+            }
+
+        });
+    }
+
+
+
+}
+
+$(document).ready(function () {
+    $('#ref').click(function () {
+        $('#myCheck1').prop('checked', false);
+        $('#myCheck2').prop('checked', false);
+        $('#myCheck3').prop('checked', false);
+        $('#slide').val(0);
+        $('#range').text(0);
+    });
+    $.get("http://localhost/cmp/getRandom16.php", function (data) {
+        var images = data.images;
+        for (var i = 0, max = images.length; i < max; i++) {
+            $(".result").prepend($('<img>', {id: images[i], class: "img-rand", src: 'http://localhost/cmp/dataset/' + images[i]}))
+        }
+    });
+    $(document).on("click", ".img-rand", function (event) {
+//   alert(event.target.id);
+        var source = "http://localhost/cmp/dataset/" + event.target.id
+        $("#img-chosen").attr("src", source);
+        $("#img-chosen").attr("alt", event.target.id);
+    });
+});
+
+
+
+
+
+
+
+
+
